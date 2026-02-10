51c9305f04aa0181c82e7e00774c302b5f68a1de
aristides1000 <aristides1000@gmail.com>
2026-02-10 11:45:20 -0400

    Feat: Add the first part of the project

---
+++ script.js
@@ -0,0 +1,28 @@
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
+const cash = document.getElementById("cash");
+const changeDue = document.getElementById("change-due");
+const purchaseBtn = document.getElementById("purchase-btn");
+//const moneyCashRegister = 
+console.log(cid[0][1]);
+
+class CashRegister {
+  constructor() {
+    this.change = 0;
+//    this.totalInCashRegister = 
+  }
+}
+
+class Cash {
+
+}
