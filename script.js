b6ca40aa3028b2e7a831c6f72894eafa0fc3c973
aristides1000 <aristides1000@gmail.com>
2026-06-07 09:52:49 -0400

    Feat: Finish all logic successfully

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const price = 19.5;
+const price = 3.26;
 const cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
 ];
 
 const cash = document.getElementById("cash");
@@ -22,7 +22,7 @@ priceElement.textContent = price;
 class CashRegister {
   constructor() {
     this.change = 0;
-    this.totalInCashRegister = cid.reduce((acc, num) => acc + num[1], 0).toFixed(2);
+    this.cidCopy = [...cid];
     this.changeCurrency = [
       ['PENNY', 0, 0.01],
       ['NICKEL', 0, 0.05],
@@ -36,29 +36,10 @@ class CashRegister {
     ];
   }
 
-/*   checkChange(amount) {
-    if (amount < 0) {
-      alert ("Customer does not have enough money to purchase the item");
-      return;
-    }
-
-    if (Number(amount) === 0) {
-      changeDue.textContent = '';
-      changeDue.textContent = 'No change due - customer paid with exact cash';
-      return;
-    }
-
-    if (this.totalInCashRegister < amount) {
-      changeDue.textContent = '';
-      changeDue.textContent = 'Status: INSUFFICIENT_FUNDS';
-      return;
-    }
-  } */
-
   cashDrawerRender() {
     cashDrawer.textContent = '';
 
-    cid.forEach(unit => {
+    this.cidCopy.forEach(unit => {
       cashDrawer.innerHTML += `<p>${unit[0]}: $${unit[1]}</p>`;
     });
   }
@@ -68,8 +49,12 @@ class CashRegister {
   }
 
   update() {
+    this.cidCopy = [...cid];
+    this.totalInCashRegister = Number(this.cidCopy.reduce((acc, num) => acc + num[1], 0).toFixed(2));
+    this.resetValuesChangeCurrency();
+
     this.change = Number(cash.value) - price;
-    //this.checkChange(this.change);
+    this.change = Number(this.change.toFixed(2));
 
     if (this.change < 0) {
       alert ("Customer does not have enough money to purchase the item");
@@ -88,25 +73,19 @@ class CashRegister {
       return;
     }
 
-
     let currencyLength = this.changeCurrency.length;
     for (let i = currencyLength - 1; i >= 0; i--) {
       debugger;
       if(Math.trunc(this.change / this.changeCurrency[i][2])) {
-        if (cid[i][1] <= this.change) {
-          this.changeCurrency[i][1] = cid[i][1];
-          this.change = (this.change - this.changeCurrency[i][1]).toFixed(2);
+        if (this.cidCopy[i][1] <= this.change) {
+          this.changeCurrency[i][1] = this.cidCopy[i][1];
+          this.change = Number((this.change - this.changeCurrency[i][1]).toFixed(2));
         } else {
-          this.changeCurrency[i][1] = Number(Math.trunc(this.change / this.changeCurrency[i][2]) * this.changeCurrency[i][2]).toFixed(2);
-          this.change = (this.change - this.changeCurrency[i][1]).toFixed(2);
+          this.changeCurrency[i][1] = Number((Math.trunc(this.change / this.changeCurrency[i][2]) * this.changeCurrency[i][2]).toFixed(2));
+          this.change = Number((this.change - this.changeCurrency[i][1]).toFixed(2));
         }
       }
-
-      /* console.log(this.changeCurrency[i][0]);
-      console.log(this.change); */
     }
-    /* console.log(this.changeCurrency);
-    console.log(this.change); */
 
     if (this.change > 0) {
       changeDue.textContent = '';
@@ -115,16 +94,28 @@ class CashRegister {
       return;
     }
 
-    const totalChange = this.changeCurrency.reduce((acc, num) => acc + num[1], 0);
+    this.change = Number(cash.value) - price;
 
-    if (totalChange) {
-      changeDue.textContent = 'Status: OPEN';
+    let changeArray = [];
+
+    const isClosed = this.totalInCashRegister === Number((Number(cash.value) - price).toFixed(2));
+
+    for (let i = currencyLength - 1; i >= 0; i--) {
+      if (isClosed) {
+        if (this.cidCopy[i][1] > 0) {
+          changeArray.push(`${this.changeCurrency[i][0]}: $${this.changeCurrency[i][1]}`);
+        }
+        this.cidCopy[i][1] = 0;
+      } else if (this.changeCurrency[i][1] > 0) {
+        changeArray.push(`${this.changeCurrency[i][0]}: $${this.changeCurrency[i][1]}`);
+        this.cidCopy[i][1] = Number((this.cidCopy[i][1] - this.changeCurrency[i][1]).toFixed(2));
+      }
     }
 
-    for (let i = 0; i <= currencyLength - 1; i++) {
-      if (this.changeCurrency[i][1] > 0) {
-        changeDue.innerHTML += ` ${this.changeCurrency[i][0]}: $${this.changeCurrency[i][1]}`;
-      }
+    if (this.totalInCashRegister === Number((Number(cash.value) - price).toFixed(2))) {
+      changeDue.textContent = `Status: CLOSED ${changeArray.join(' ')}`;
+    } else {
+      changeDue.textContent = `Status: OPEN ${changeArray.join(' ')}`;
     }
 
     this.cashDrawerRender();
